56bc25424d189f9bbd1b551569769d236bc09a47
Surender Kumar <surenderprajapati4435@gmail.com>
2024-03-28 17:28:03 +0530

    My end t0 end Project

---
+++ setup.py
@@ -0,0 +1,32 @@
+## BY this setup.py file we we build a packeges of our whole Project
+
+from setuptools import find_packages,setup
+from typing import List
+
+
+
+def get_requirements(file_path:str)->List[str]:
+
+    '''
+    this function return the list of requirements
+
+    '''
+    requirements = []
+
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace('\n',"") for req in requirements]
+
+    return requirements
+
+
+setup(
+
+      name='ML_PROJECT',
+      version='0.0.1',
+      author='Surender Kumar',
+      author_email='surenderpelak4435@gmail.com',
+      packages= find_packages(),
+      install_requires= get_requirements("requirements.txt")
+
+     )
