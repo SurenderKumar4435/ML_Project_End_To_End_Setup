052874421b81b122f534f2dd07d3484cbe3a4d51
Surender Kumar <surenderprajapati4435@gmail.com>
2024-04-02 21:14:42 +0530

    Commited Final

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ from src.exception import CustomException
 import sys
 from src.components.data_ingestion import DataIngestion
 from src.components.data_ingestion import DataIngestionConfig
+from flask import Flask,request,jsonify,render_template
 
 
 
-
+'''
 
 if __name__=="__main__":
     logging.info("The execution has started")
@@ -19,6 +20,7 @@ if __name__=="__main__":
         logging_info("custom Exception")
         raise CustomException(e,sys)
 
+'''
 
 
 
@@ -44,8 +46,8 @@ app  = Flask(__name__)
 
 
 ## import pkl file----------------->>>>>>>>>
-reg_model = pickle.load(open("model/reg.pkl","rb"))
-standard_scaler = pickle.load(open("model/scaler.pkl","rb"))
+process_model = pickle.load(open("notebook/preprocessor.pkl","rb"))
+standard_scaler = pickle.load(open("notebook/scaler2.pkl","rb"))
 
 
 ## Route for home page ---------->>>>>>>>>>>
@@ -57,24 +59,20 @@ def index():
 @app.route("/predictdata",methods=["GET","POST"])
 def predict_datapoint():
     if request.method=="POST":
-         Order_ID = float(request.form.get("Order_ID"))
-         Quantity_Ordered= float(request.form.get("Quantity_Ordered"))
-         Price_Each = float(request.form.get("Price_Each"))
-         Month = float(request.form.get("Month"))
-         Day = float(request.form.get("Day"))
-         Year = float(request.form.get("Year"))
-         Hours= float(request.form.get("Hours"))
-         Minute = float(request.form.get("Minute"))
-         A= str(request.form.get("A"))
-         B = str(request.form.get("B"))
-         C= str(request.form.get("C"))
-         D = str(request.form.get("D"))
+         Pregnancies= float(request.form.get("Pregnancies"))
+         Glucose= float(request.form.get("Glucose"))
+         BloodPressure = float(request.form.get("BloodPressure"))
+         SkinThickness = float(request.form.get("SkinThickness"))
+         Insulin = float(request.form.get("Insulin"))
+         BMI = float(request.form.get("BMI"))
+         DiabetesPedigreeFunction = float(request.form.get("DiabetesPedigreeFunction"))
+         Age= str(request.form.get("Age"))
          
          
          
 
-         new_data_scaled = standard_scaler.transform([[Order_ID,Quantity_Ordered,Price_Each,Month,Day,Year,Hours,Minute,A,B,C,D]])
-         result = reg_model.predict(new_data_scaled)
+         new_data_scaled = standard_scaler.transform([[Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age]])
+         result = process_model.predict(new_data_scaled)
 
          return render_template("home.html",result=result[0])
 
